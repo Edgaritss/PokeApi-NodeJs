8e5c5a2e07ddb7fbd52a81857053f6ee3d9ff607
Edgar Rojas <garos19480@gmail.com>
2022-04-08 18:45:03 -0500

    update 2

--- poke.js
+++ poke.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const express = require('express') ;
 const app =express();
-var port = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 let url ='https://pokeapi.co/api/v2/';
 
 
